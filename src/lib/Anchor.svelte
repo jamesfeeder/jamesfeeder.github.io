be0bc94be800122bcb3eef3a0fe35797f69f9fd5
Jetsadakorn Maliwan <jatsadakorn555@gmail.com>
2023-09-21 00:50:03 +0700

    remove unused feature

--- src/lib/Anchor.svelte
+++ src/lib/Anchor.svelte
@@ -8,8 +8,6 @@
 	export let link;
 	/**  @type {boolean} Open in new tab?*/
 	export let openNewTab = false;
-	/** @type {string} Additional Class*/
-	export let additionalClass = '';
 	/**  @type {boolean} Disable icon*/
 	export let disableIcon = false;
 </script>
@@ -20,12 +18,12 @@
 	on:click={() => {
 		targetUrlStore.update((url) => (url = link));
 	}}
-	class={`group relative flex items-center gap-1 border-b-2 border-transparent font-medium text-blue-600 transition hover:z-10 hover:border-current hover:bg-slate-50   ${additionalClass}`}
+	class="group relative flex items-center gap-1 border-b-2 border-transparent font-medium text-blue-600 transition hover:z-10 hover:border-current hover:bg-slate-50"
 	>{label}
 
 	{#if !disableIcon}
 		<div
-			class={`absolute right-0 top-0 isolate -z-10 box-content flex h-full translate-x-0 items-center justify-center border-b-2 border-current pl-1 text-transparent transition-all group-hover:translate-x-full group-hover:bg-slate-50 group-hover:text-inherit  ${additionalClass}`}
+			class="absolute right-0 top-0 isolate -z-10 box-content flex h-full translate-x-0 items-center justify-center border-b-2 border-current pl-1 text-transparent transition-all group-hover:translate-x-full group-hover:bg-slate-50 group-hover:text-inherit"
 		>
 			<slot
 				>{#if openNewTab}
