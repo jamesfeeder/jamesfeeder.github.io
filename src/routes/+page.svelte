89c9c03cb84c084ea41518dc4b2028f3f4f6a12c
Jetsadakorn Maliwan <jatsadakorn555@gmail.com>
2023-12-05 15:55:35 +0700

    style: center element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,14 +31,14 @@
 	`}
 >
 	<div class="p-6 pb-24">
-		<h1 class="text-3xl font-semibold text-slate-800 sm:text-[2rem] md:text-[2.5rem]">
+		<h1 class="text-3xl font-semibold text-slate-800 sm:text-[2rem] md:text-[2.5rem] text-center">
 			Jetsadakorn Maliwan
 		</h1>
-		<div class="flex flex-wrap items-center gap-1.5 py-4">
+		<div class="flex flex-wrap items-center justify-center gap-1.5 py-4">
 			<p class="font-light text-slate-500">Software Developer /</p>
 			<p class="font-light text-slate-500">Front-End Web Developer</p>
 		</div>
-		<div class="flex flex-wrap items-center gap-6 gap-y-1 sm:gap-8">
+		<div class="flex flex-wrap items-center justify-center gap-6 gap-y-1 sm:gap-8">
 			<Anchor label="GitHub Profile" link="https://github.com/jamesfeeder" openNewTab />
 			<Anchor label="Curriculum Vitae" link="/cv" />
 		</div>
