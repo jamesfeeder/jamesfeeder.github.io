3287c348a133f923279c5ca79794cbaea970a29b
Jetsadakorn Maliwan <jatsadakorn555@gmail.com>
2024-07-19 20:42:42 +0700

    change anchor button style

--- src/lib/Anchor.svelte
+++ src/lib/Anchor.svelte
@@ -18,12 +18,12 @@
 	on:click={() => {
 		targetUrlStore.update((url) => (url = link));
 	}}
-	class="group relative flex items-center gap-1 border-b-2 border-transparent font-medium text-blue-600 transition hover:z-10 hover:border-current hover:bg-slate-50"
+	class="group relative flex items-center gap-1 rounded-full bg-inherit px-2 py-1 font-medium text-blue-600 transition duration-300 hover:z-10 hover:border-current hover:bg-blue-600 hover:text-white hover:duration-75 active:scale-95"
 	>{label}
 
 	{#if !disableIcon}
 		<div
-			class="absolute right-0 top-0 isolate -z-10 box-content flex h-full translate-x-0 items-center justify-center border-b-2 border-current pl-1 text-transparent transition-all group-hover:translate-x-[98%] group-hover:bg-slate-50 group-hover:text-inherit"
+			class="absolute right-0 top-0 isolate -z-10 box-content flex h-full translate-x-0 items-center justify-center rounded-full bg-blue-600 pl-5 pr-2 text-white opacity-0 transition-all group-hover:translate-x-[45%] group-hover:opacity-100 group-hover:duration-150"
 		>
 			<slot
 				>{#if openNewTab}
